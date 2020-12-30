12e9d384b3c0033abb98d7e1ecd5684d7f600abd
Filipe Gomes <fgdl.py91@gmail.com>
2020-12-30 11:14:03 -0300

    Added new codes

--- src/arrays.js
+++ src/arrays.js
@@ -12,3 +12,237 @@ console.log(names.length)
 const findName = names.findIndex(el => el === 'Wendy')
 names.splice(2, 1)
 console.log(names)
+
+const array = [1, 2, 3]
+array.push(4)
+array[0] = 'Filipe'
+console.log(array)
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for of
+for (let numero of numeros) {
+  if (numero === 2) {
+    console.log("Pulei o numero 2");
+    continue;
+  }
+
+  console.log(numero);
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...");
+    break;
+  }
+}
+
+console.log("---- **** ----");
+
+// for of
+for (let i of numeros) {
+  let numero = numeros[i];
+
+  if (numero === 2) {
+    console.log("Pulei o numero 2");
+    continue;
+  }
+
+  console.log(numero);
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...");
+    break;
+  }
+}
+
+console.log("---- **** ----");
+
+// while
+let i = 0;
+while (i < numeros.length) {
+  let numero = numeros[i];
+
+  if (numero === 2) {
+    console.log("Pulei o numero 2");
+    i++;
+    continue;
+  }
+
+  console.log(numero);
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...");
+    i++;
+    break;
+  }
+
+  i++;
+}
+
+console.log("---- **** ----");
+
+// do while
+let n = 0;
+do {
+  let numero = numeros[n];
+
+  if (numero === 2) {
+    console.log("Pulei o numero 2");
+    n++;
+    continue;
+  }
+
+  console.log(numero);
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...");
+    n++;
+    break;
+  }
+
+  n++;
+} while (n < numeros.length);
+
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const [firstNum, secondNum, ...rest] = num1
+console.log(firstNum, secondNum)
+console.log(rest)
+
+const num2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+console.log(num2[2] [1]);
+
+const num3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+const[,[,,six]] = num3;
+console.log(six);
+
+const num4 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+const[list1, list2, list3] = num4;
+console.log(list3);
+console.log(list3[0])
+
+const nome = ['Marie', 'Leclarier', 'Austin']
+
+for(let i = 0; i < nome.length; i++){
+    console.log(nome[i])
+}
+
+for(let i in nome) {
+    console.log(nome[i])
+}
+
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+nome.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array)
+})
+
+const animeCharacter = ['Monkey', 'D.', 'Luffy']
+
+for (let i = 0; i < animeCharacter.length; i++) {
+  console.log(animeCharacter[i])
+}
+
+for (let i in animeCharacter) {
+  console.log([i])  // return indexes
+  console.log(animeCharacter[i])  // returns the name
+}
+
+for (let valor of animeCharacter) {
+  console.log(valor)
+}
+
+animeCharacter.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+})
+
+const teacher = {
+    name: 'Eikichi',
+    surname: 'Onizuka'
+}
+
+for (let key in teacher) {
+    console.log(key, teacher[key]);
+}
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for of
+for (let numero of numeros) {
+  if (numero === 2) {
+    console.log("Pulei o numero 2")
+    continue
+  }
+
+  console.log(numero)
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...")
+    break;
+  }
+}
+
+console.log("---- **** ----")
+
+// for of
+for (let i of numeros) {
+  let numero = numeros[i]
+
+  if (numero === 2) {
+    console.log("Pulei o numero 2")
+    continue
+  }
+
+  console.log(numero)
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...")
+    break;
+  }
+}
+
+console.log("---- **** ----")
+
+// while
+let i = 0;
+while (i < numeros.length) {
+  let numero = numeros[i]
+
+  if (numero === 2) {
+    console.log("Pulei o numero 2")
+    i++
+    continue
+  }
+
+  console.log(numero)
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...")
+    i++
+    break
+  }
+
+  i++
+}
+
+console.log("---- **** ----");
+
+// do while
+let n = 0
+do {
+  let numero = numeros[n];
+
+  if (numero === 2) {
+    console.log("Pulei o numero 2")
+    n++
+    continue
+  }
+
+  console.log(numero);
+
+  if (numero === 7) {
+    console.log("7 encontrado, saindo...")
+    n++
+    break
+  }
+
+  n++
+} while (n < numeros.length)
--- src/objects.js
+++ src/objects.js
@@ -19,3 +19,126 @@ delete person.hobbies
 
 console.log(person)
 person.greet()
+
+const person1 = {
+  name: 'Jackie',
+  lastname: 'Chan',
+  age: 65
+}
+
+console.log(person1.name)
+console.log(person1.lastname)
+console.log(person1.age)
+
+function creatPerson(name, lastname, age) {
+  return { name, lastname, age }
+}
+
+const person2 = creatPerson('Jackie', 'Chan', 65)
+const person3 = creatPerson('Marry', 'Polsk', 18)
+const person4 = creatPerson('Victoria', 'Kalmar', 19)
+
+console.log(person1.name, person1.lastname, person1.age)
+console.log(person2.name, person2.lastname, person2.age)
+console.log(person3.name, person3.lastname, person3.age)
+console.log(person4.name, person4.lastname, person4.age)
+
+const person5 = {
+  name: 'Jackie',
+  lastname: 'Chan',
+  age: 65,
+  speak() {
+    console.log(`My current age is ${this.age}.`)
+  },
+
+  increaseAge() {
+    this.age++
+  }
+}
+
+person5.speak()
+person5.increaseAge()
+person5.speak()
+
+const data1 = new Date('1991-09-18 23:53:00')
+const weekday1 = data1.getDay()
+let weekdayText1
+
+switch (weekday1) {
+  case 0:
+    weekdayText1 = 'Sunday';
+    break;
+  case 1:
+    weekdayText1 = 'Monday';
+    break;
+  case 2:
+    weekdayText1 = 'Tuesday';
+    break;
+  case 3:
+    weekdayText1 = 'Wednesday';
+    break;
+  case 4:
+    weekdayText1 = 'Thursday';
+    break;
+  case 5:
+    weekdayText1 = 'Friday';
+    break;
+  case 6:
+    weekdayText1 = 'Saturday';
+    break;
+  default:
+    weekdayText1 = '';
+    break;
+}
+
+console.log(weekday1, weekdayText1)
+
+function getweekdayText2(weekday) {
+  let weekdayText2;
+  switch (weekday2) {
+    case 0:
+      weekdayText2 = 'Sunday';
+      return weekdayText2;
+    case 1:
+      weekdayText2 = 'Monday';
+      return weekdayText2;
+    case 2:
+      weekdayText2 = 'Tuesday';
+      return weekdayText2;
+    case 3:
+      weekdayText2 = 'Wednesday';
+      return weekdayText2;
+    case 4:
+      weekdayText2 = 'Thursday';
+      return weekdayText2;
+    case 5:
+      weekdayText2 = 'Friday';
+      return weekdayText2;
+    case 6:
+      weekdayText2 = 'Saturday';
+      return weekdayText2;
+    default:
+      weekdayText2 = '';
+      return weekdayText2;
+  }
+}
+
+const data2 = new Date('1991-09-18 23:53:00')
+const weekday2 = data2.getDay()
+const weekdayText2 = getweekdayText2(weekday2)
+
+console.log(weekday2, weekdayText2)
+
+const student = {
+  firstName: 'Filipe',
+  surname: 'Gomes',
+  age: 29,
+  address: {
+      street: 'Av. Ayrton Senna',
+      number: 369
+  }
+};
+
+// Destructuring assignment
+const {firstName, surname, age, address: {street, number}} = student;
+console.log(firstName, surname, age, street, number);
